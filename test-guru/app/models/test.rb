32238a489fbf8b3ba1bdabd3b61767153f579187
GraberGT <rogerpluscom@gmail.com>
2020-11-24 23:56:49 +0600

    test_commit

--- test-guru/app/models/test.rb
+++ test-guru/app/models/test.rb
@@ -1,4 +1,8 @@
 class Test < ApplicationRecord
+belongs_to :category
+has_many :questions
+has_and_belongs_to_many :users
+
   def self.titles(category)
     joins("JOIN categories ON tests.category_id = categories.id")
       .where(categories: { category: category })
--- test-guru/app/models/user.rb
+++ test-guru/app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+
+  has_and_belongs_to_many :tests
+
   def passed_tests_by_level(level)
     Test.joins("JOIN test_histories ON test_histories.test_id = tests.id")
       .where(level: level, test_histories: { user_id: self.id })
