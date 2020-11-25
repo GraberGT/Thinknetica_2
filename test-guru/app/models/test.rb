35fbc3b5212c8caefc3af596f73d5e3f39decc97
GraberGT <rogerpluscom@gmail.com>
2020-11-26 01:28:42 +0600

    second_commit

--- test-guru/app/models/test.rb
+++ test-guru/app/models/test.rb
@@ -1,10 +1,13 @@
 class Test < ApplicationRecord
 belongs_to :category
+belongs_to :author, class_name: "User", optional: true
 has_many :questions
+has_many :test_histories
 has_and_belongs_to_many :users
 
+
   def self.titles(category)
-    joins("JOIN categories ON tests.category_id = categories.id")
+    joins(:category)
       .where(categories: { category: category })
       .order(title: :desc)
       .pluck(:title)
--- test-guru/app/models/user.rb
+++ test-guru/app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
-
-  has_and_belongs_to_many :tests
+  has_many :test_histories
+  has_and_belongs_to_many :tests, through: :test_histories
+  has_many :created_tests, class_name: "Test", foreign_key: "author_id"
 
   def passed_tests_by_level(level)
-    Test.joins("JOIN test_histories ON test_histories.test_id = tests.id")
+    Test.joins(:test)
       .where(level: level, test_histories: { user_id: self.id })
       .distinct
   end
