a289c7d2c00e481f3946999f02b92223c9364615
GraberGT <rogerpluscom@gmail.com>
2020-12-03 23:11:00 +0600

    eight_commit

--- test-guru/app/models/user.rb
+++ test-guru/app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_and_belongs_to_many :tests
-  has_many :create_tests
+  has_many :create_tests, class_name: "Test", foreign_key: "author_id",
+  dependent: :nullify
 
   def passed_tests_by_level(level)
     tests.where(level: level)
