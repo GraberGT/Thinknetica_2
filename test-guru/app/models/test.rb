76e1b09c3e86ab790edfe850388aae8fd968206c
GraberGT <rogerpluscom@gmail.com>
2020-12-04 22:57:44 +0600

    nine_commit

--- test-guru/app/models/test.rb
+++ test-guru/app/models/test.rb
@@ -4,7 +4,6 @@ belongs_to :author, class_name: "User", optional: true
 has_many :questions
 has_and_belongs_to_many :users
 
-
   def self.titles(category)
     joins("JOIN categories ON tests.category_id = categories.id")
       .where(categories: { category: category })
