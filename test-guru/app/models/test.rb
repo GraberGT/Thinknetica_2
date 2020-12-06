d7bad6406c7afb8532d06cb570564e127022e027
GraberGT <rogerpluscom@gmail.com>
2020-12-06 23:07:40 +0600

    first_commit

--- test-guru/app/models/test.rb
+++ test-guru/app/models/test.rb
@@ -4,10 +4,19 @@ belongs_to :author, class_name: "User", optional: true
 has_many :questions
 has_and_belongs_to_many :users
 
-  def self.titles(category)
-    joins("JOIN categories ON tests.category_id = categories.id")
-      .where(categories: { category: category })
-      .order(title: :desc)
-      .pluck(:title)
-  end
+validates :title, presence: true, 
+uniqueness: { scope: :level, message: "should be one level for title" }
+validates :level, numericality: { greater_than_or_equal_to: 0}
+
+scope :easy, -> { where(level: 0..1) }
+scope :medium, -> { where(level: 2..4) }
+scope :hard, -> { where(level: 5..Float::INFINITY) }
+
+scope :by_level, -> (level) { where(level: level) }
+
+scope :titles_by_category, -> (category) {
+select(:title).joins(:category).where(categories: {title: category})
+  .order(title: :desc)
+}
+
 end
--- test-guru/app/models/user.rb
+++ test-guru/app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   has_many :created_tests, class_name: "Test", foreign_key: "author_id",
   dependent: :nullify
 
+  validates :name, :email, presence: true
+
   def passed_tests_by_level(level)
     tests.where(level: level)
   end
