ce1c008b65a32ea0222100df9449223279a5c4ad
GraberGT <rogerpluscom@gmail.com>
2020-12-11 23:41:57 +0600

    second_commit_1

--- test-guru/app/models/test.rb
+++ test-guru/app/models/test.rb
@@ -4,9 +4,9 @@ belongs_to :author, class_name: "User", optional: true
 has_many :questions
 has_and_belongs_to_many :users
 
-validates :title, presence: true, 
-uniqueness: { scope: :level, message: "should be one level for title" }
-validates :level, numericality: { greater_than_or_equal_to: 0}
+validates :title, :level, presence: true
+validates :title, uniqueness: { scope: :level }
+validates :level, numericality: { only_integer: true, :greater_than_or_equal_to => 0 }
 
 scope :easy, -> { where(level: 0..1) }
 scope :medium, -> { where(level: 2..4) }
