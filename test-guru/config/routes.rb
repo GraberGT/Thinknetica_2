513226fd6c74559c9fc8a2be864108b75d5da003
GraberGT <rogerpluscom@gmail.com>
2021-01-27 01:37:30 +0600

    second_commit

--- test-guru/config/routes.rb
+++ test-guru/config/routes.rb
@@ -17,6 +17,6 @@ Rails.application.routes.draw do
       get :result
     end
   end
-eЫnd
+end
 
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
--- test-guru/db/schema.rb
+++ test-guru/db/schema.rb
@@ -37,7 +37,7 @@ ActiveRecord::Schema.define(version: 2021_01_25_183153) do
   create_table "test_passages", force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "test_id", null: false
-    t.integer "current_question_id", null: false
+    t.integer "current_question_id"
     t.integer "correct_questions", default: 0
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -74,7 +74,7 @@ ActiveRecord::Schema.define(version: 2021_01_25_183153) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  add_foreign_key "test_passages", "current_questions"
+  add_foreign_key "test_passages", "questions", column: "current_question_id"
   add_foreign_key "test_passages", "tests"
   add_foreign_key "test_passages", "users"
   add_foreign_key "tests", "categories"
