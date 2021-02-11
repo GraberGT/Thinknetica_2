1400123419ad7148d8b7cc2123e315a5f6e5733a
GraberGT <rogerpluscom@gmail.com>
2021-02-11 11:47:03 +0600

    nine_commit

--- test-guru/app/controllers/application_controller.rb
+++ test-guru/app/controllers/application_controller.rb
@@ -1,32 +1,38 @@
 class ApplicationController < ActionController::Base
-  before_action :set_locale
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  #before_action :set_locale
+  #before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def default_url_options(options={})
-    { lang: (I18n.locale if I18n.locale != I18n.default_locale) }
+  #def default_url_options(options={})
+    #{ lang: (I18n.locale if I18n.locale != I18n.default_locale) }
+  #end
+  def after_sign_in_path_for(current_user)
+    if current_user.admin?
+      admin_root_path
+    else
+      super
+    end
   end
+  #private
 
-  private
+  #def set_locale
+    #available = I18n.locale_available?(params[:lang])
+    #I18n.locale = available ? params[:lang] : I18n.default_locale
+  #end
 
-  def set_locale
-    available = I18n.locale_available?(params[:lang])
-    I18n.locale = available ? params[:lang] : I18n.default_locale
-  end
-
-  def configure_permitted_parameters
-    permitted_list = %i[first_name last_name]
-    devise_parameter_sanitizer.permit(:sign_up, keys: permitted_list)
-    devise_parameter_sanitizer.permit(:account_update, keys: permitted_list)
-  end
+  #def configure_permitted_parameters
+    #permitted_list = %i[first_name last_name]
+    #devise_parameter_sanitizer.permit(:sign_up, keys: permitted_list)
+    #devise_parameter_sanitizer.permit(:account_update, keys: permitted_list)
+  #end
 
   # custom devise redirect path for Admin login
-  def after_sign_in_path_for(resource)
-    (admin_tests_path if current_user&.is_a?(Admin)) || super
-  end
+  #def after_sign_in_path_for(resource)
+    #(admin_tests_path if current_user&.is_a?(Admin)) || super
+  #end
 
   # custom flash notice on devise sign_in
-  def sign_in(resource_or_scope, *args)
-    flash[:notice] = t('app.hello', name: current_user.first_name)
-    super
-  end
+  #def sign_in(resource_or_scope, *args)
+    #flash[:notice] = t('app.hello', name: current_user.first_name)
+    #super
+  #end
 end
