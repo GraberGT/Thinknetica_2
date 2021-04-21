399fb5acd290f1b98a941230e8bb5d0acd2568b7
GraberGT <rogerpluscom@gmail.com>
2021-04-22 01:03:33 +0600

    first commit

--- test-guru/app/models/question.rb
+++ test-guru/app/models/question.rb
@@ -1,7 +1,7 @@
 class Question < ApplicationRecord
-  belongs_to :test
+  belongs_to :test, dependent: :destroy
   has_many :answers, dependent: :destroy
-  has_many :gists
+  has_many :gists, dependent: :destroy
 
   validates :title, presence: true
 end
--- test-guru/db/schema.rb
+++ test-guru/db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_25_172605) do
+ActiveRecord::Schema.define(version: 2021_04_16_184347) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "answers", force: :cascade do |t|
     t.string "title", null: false
